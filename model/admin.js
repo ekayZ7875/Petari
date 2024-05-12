b9bb079e69786db07f37eb8459dd4b734ea15f5e
[Eklavya Singh] <[eklavyasinghparihar7875@gmail.com]>
2024-05-13 01:00:03 +0530

    fix-some-bugs

--- model/admin.js
+++ model/admin.js
@@ -1,16 +1,12 @@
-
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const AdminSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  fullName: String,
+  Mobile: Number,
+});
 
-    username: String,
-      password: String,
-   fullName: String,
-
-      Mobile: Number,
-})
-
-const Admin = mongoose.model('Admin',AdminSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
 
 module.exports = Admin;
